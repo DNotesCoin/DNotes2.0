0292e74af11d6724dbb579436ba099e1dc5ba285
KemBits <bitalize@gmail.com>
2016-08-08 19:14:22 +0530

    [PoW] We require mature coins

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -119,7 +119,7 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
-        nLastPOWBlock = 10;
+        nLastPOWBlock = 1000;
     }
 
     virtual const CBlock& GenesisBlock() const { return genesis; }
