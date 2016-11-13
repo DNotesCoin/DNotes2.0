271c5a10732abc3568464c0861e2fe3a75262a5b
Steven <steven@sigwo.com>
2016-11-13 06:28:10 -0500

    Set hardfork date
    Sat, 19 Nov 2016 00:00:00 GMT

--- src/main.h
+++ src/main.h
@@ -59,7 +59,7 @@ inline bool IsProtocolV1RetargetingFixed(int nHeight) { return TestNet() || nHei
 inline bool IsProtocolV2(int nHeight) { return TestNet() || nHeight > 0; }
 inline bool IsProtocolV3(int64_t nTime) { return TestNet() || nTime > 1470467000; }
 
-inline bool IsDriftReduced(int64_t nTime) { return TestNet() || nTime > 1480118400; } // Drifting Bug Fix
+inline bool IsDriftReduced(int64_t nTime) { return TestNet() || nTime > 1479513600; } // Drifting Bug Fix, hardfork on Sat, 19 Nov 2016 00:00:00 GMT
 
 inline int64_t TestingDrift(int64_t nTime) { return nTime + 128 * 60 * 60; }
 inline int64_t MainNetDrift(int64_t nTime) { return nTime + 15; }
