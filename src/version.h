6d6714347d467816e57708fe00d9306ccc50bc9a
Steven <steven@sigwo.com>
2016-11-13 08:55:16 -0500

    Allow current clients
    so we have seamless hardfork. Will update after hardfork to reject all older clients.

--- src/version.h
+++ src/version.h
@@ -36,7 +36,7 @@ static const int PROTOCOL_VERSION = 70000;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60019;
+static const int MIN_PEER_PROTO_VERSION = 60018;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -53,7 +53,7 @@ static const int BIP0031_VERSION = 60000;
 static const int MEMPOOL_GD_VERSION = 60002;
 
 // reject blocks with non-canonical signatures starting from this version
-static const int CANONICAL_BLOCK_SIG_VERSION = 60019;
+static const int CANONICAL_BLOCK_SIG_VERSION = 60018;
 static const int CANONICAL_BLOCK_SIG_LOW_S_VERSION = 70000;
 
 #endif
