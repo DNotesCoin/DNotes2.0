e371432a7b3a27d69970c32523cb0cfbd4e7ab4d
dev0tion <dev0tion@hotmail.com>
2017-05-10 16:07:03 +0200

    add vSeed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -167,6 +167,7 @@ public:
 
         vFixedSeeds.clear();
         vSeeds.clear();
+        vSeeds.push_back(CDNSSeedData("stratisplatform.com", "testnode1.stratisplatform.com"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 65); // stratis test net start with T
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 196);
